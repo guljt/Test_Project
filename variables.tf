41423a500cfb9c35870616e6af6ee9add3825855
Guljeet Singh Talwar <guljeet.talwar@altair.com>
2022-09-16 12:42:51 +0530

    updated the files

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "mykeypair"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "launch-wizard-1"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
